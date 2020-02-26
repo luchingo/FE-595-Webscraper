96496bde51db7e48f5ea1ea52a73687e47718865
luchingo <60360781+luchingo@users.noreply.github.com>
2020-02-25 22:51:38 -0500

    Update Assignment_2.py
    Updated with corrections

--- Assignment_2.py
+++ Assignment_2.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 
 
 def scraper(www, maxiter):
-    Company = open("Company.txt", "w+")
+    company = open("Company.txt", "w+")
     for i in range(maxiter):
         r = rq.get(www)
         content = bs(r.text, "html.parser")
@@ -11,11 +11,11 @@ def scraper(www, maxiter):
         for tag in tags:
             tag2 = tag.text
             if "Name" in tag2:
-                mytag = tag2.replace("Name: ", "")
-                Company.write(str(i) + ")" + mytag + "\n")
+                myname = tag2.replace("Name: ", "")
             if "Purpose" in tag2:
-                mytag = tag2.replace("Purpose: ", "")
-                Company.write("  " + mytag + "\n")
+                mypurpose = tag2.replace("Purpose: ", "")
+                company.write(f"{i}) {myname} \n {mypurpose}\n")
+    company.close()
 
 
 scraper(" http://18.207.92.139:8000/random_company", 50)
